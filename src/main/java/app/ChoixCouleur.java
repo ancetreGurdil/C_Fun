91b74bfb5fa8d02de1f80a5a2863965057d62242
guillaume <guillaume.kerherve@orange.fr>
2022-04-13 13:11:32 +0200

    projet maven

---
+++ src/main/java/app/ChoixCouleur.java
@@ -0,0 +1,15 @@
+package app;
+
+public class ChoixCouleur {
+	private Couleur couleur;
+
+	public ChoixCouleur(final double etat){
+
+		couleur = Couleur.rouge;
+	}
+	
+	public Couleur getCouleur() {
+
+		return couleur;
+	}
+}
---
+++ src/main/java/app/Couleur.java
@@ -0,0 +1,7 @@
+package app;
+
+public enum Couleur {
+	  rouge,
+	  vert ,
+	  orange ;
+}
